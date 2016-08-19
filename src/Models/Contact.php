19dfc2f8d29209f07ea7b4048bfa383a3dc3b14f
James Grose <grose.james@gmail.com>
2016-08-19 17:40:53 +1000

    Contact model docblocks

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -6,8 +6,18 @@ use Illuminate\Support\Collection;
 
 class Contact
 {
+    /**
+     * The contact ID.
+     *
+     * @var int
+     */
     public $vid;
 
+    /**
+     * A collection of properties the contact has.
+     *
+     * @var Collection
+     */
     public $properties;
 
     /**
@@ -21,11 +31,20 @@ class Contact
         $this->properties = $this->mapProperties($object->properties);
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return '';
     }
 
+    /**
+     * Check if the property is accessible.
+     *
+     * @param $property
+     * @return bool
+     */
     public function __isset($property)
     {
         if ($this->wantsId($property)) {
@@ -35,6 +54,23 @@ class Contact
         return $this->properties->has($property);
     }
 
+    /**
+     * Remove the property from the model.
+     *
+     * @param $property
+     * @return void
+     */
+    public function __unset($property)
+    {
+        $this->properties->forget($property);
+    }
+
+    /**
+     * Get a property from the contact.
+     *
+     * @param $property
+     * @return mixed
+     */
     public function __get($property)
     {
         if ($this->wantsId($property)) {
@@ -44,11 +80,23 @@ class Contact
         return $this->properties->get($property);
     }
 
+    /**
+     * Set a property on the contact.
+     *
+     * @param $property
+     * @param $value
+     */
     public function __set($property, $value)
     {
-
+        $this->properties->put($property, $value);
     }
 
+    /**
+     * Map property values to their key.
+     *
+     * @param $properties
+     * @return Collection
+     */
     private function mapProperties($properties)
     {
         $properties = Collection::make($properties);
@@ -58,6 +106,12 @@ class Contact
         });
     }
 
+    /**
+     * Check if the requested property is an ID.
+     *
+     * @param $property
+     * @return bool
+     */
     private function wantsId($property)
     {
         return $property === 'id' || $property === 'vid';
